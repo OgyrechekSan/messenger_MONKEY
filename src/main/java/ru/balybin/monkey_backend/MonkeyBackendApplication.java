936ec6d85eca566fc6ec51a9559d04c5684bcc74
OgyrechekSan <balybin.1988@mail.ru>
2025-09-21 23:52:25 +0300

    Add security config

---
+++ src/main/java/ru/balybin/monkey_backend/MonkeyBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.balybin.monkey_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/balybin/monkey_backend/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package ru.balybin.monkey_backend.config;
+
+public class JwtConstant {
+    public final static String JWT_HEADER = "Authorization";
+    public final static String SECRET_KEY = "Peanut_Butter_Jelly_The_Long_Way";
+}
